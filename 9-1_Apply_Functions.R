fd7236b9e4575cd598f30b03cb066584056ef7c3
hachepunto <hachepunto@gmail.com>
2019-03-26 16:07:04 -0600

    clase de apply terminada

--- 9-1_Apply_Functions.R
+++ 9-1_Apply_Functions.R
@@ -1,10 +1,10 @@
 ################################################################################
 ### R BASICS WORKSHOP                                                        ###
-### PRESENTATION 9-1: Las Funciones apply de R                          ### 
-###                                                                                                                                                   ###
-### Unida de Servicios Bioinformáticos                                                                                           ###
-### Instituto Nacional de Medicina Genómica                                                                                 ###
-### Website: github.com/hachepunto/R_Basics_workshop                                                             ### 
+### PRESENTATION 9-1: Las Funciones apply de R                               ###
+###                                                                          ###
+### Unida de Servicios Bioinformáticos                                       ###
+### Instituto Nacional de Medicina Genómica                                  ###
+### Website: github.com/hachepunto/R_Basics_workshop                         ###
 ################################################################################
 
 # La familia de funciones apply es usada para aplicar una función a cada 
@@ -14,27 +14,28 @@
 # poca líneas de código y es una de las características distintivas de R como 
 # lenguaje de programación.
 
-### RECORDATORIO. Operaciones con vectores
+Las funciones de la familia apply son:
 
-# Para enterder más facilmente como trabajan las funciones apply, recordemos la
-# vectorización de operaciones:
+#		apply
+#		lapply
+#		sapply
+#		tapply
+#		vapply
+#		eapply
+#		mapply
+#		rapply
 
-# Hay operaciones que, si las aplicamos a un vector, son aplicadas a todos sus 
-# elementos.
+# Aquí solo veremos las primeras cuatro por ser las básicas.
 
-mi_vector <- 1:10
-mi_vector
+######################## apply ########################
 
-mi_vector ^ 2
+# Se utiliza la función *apply* sobre matricez. Esta funión toma cinco 
 
-
-## APPLY
-
-# Se utiliza la función *apply()* sobre matricez. Esta funión toma cinco 
-# argumentos:
+# Uso:
 #
 #	apply(X, MARGIN, FUN, ...)
 #
+# Argumentos:
 # -X: una matriz o cualquier otro objeto que sea posible coercionar a una matriz
 # -MARGIN: Define sobre qué se va ejecutar la función y toma valores entre 1 y 2:
 #	-MARGIN=1`: La manipulación se hace por renglón
@@ -45,6 +46,8 @@ mi_vector ^ 2
 
 # La funcion apply ejecuta la funcion "FUN", sobre todos las columnas 
 # (MARGIN =2) o renglones (MARGIN = 1) de una matriz.
+
+### Ejemplo sencillo
 
 # Creamos una matriz de cuatro renglones
 matriz <- matrix(1:16, nrow = 4)
@@ -67,9 +70,11 @@ sum(matriz[4, ])
 
 apply(X = matriz, MARGIN = 2, FUN = sum)
 
+### Ejemplo datos genómicos
+
 ### PREPARACIÓN DE DATOS: Comencemos por cargar unos datos de expresión de 
 # ENSEMBLE de Drosophila Los renglones son genes y hay información de cuatro 
-# tiempos (2, 6, 8 y 10) y otros metadatos.
+# tiempos (2, 6, 8 y 10 hrs) y otros metadatos.
 
 data <- read.table("Datasets/rpkm_clase.tab", header=T, sep="\t")
 
@@ -113,83 +118,186 @@ hist( apply(rpkm, 1, mean),
 			length.out = 50)
 	)
 
-##  lapply y sapply
 
-?lapply
+######################## lapply ########################
+
+# La "l" de *lapply* se refiere a "lista". La diferencia entre *lapply* y 
+# *apply* es el tipo de salida que genera cada una de ellas. La salida de 
+# *lapply* es una lista. *lapply* se puede usar sobre otros tipos de objetos
+# como data frames y listas.
+
+# La función *lapply* no necesita el parámetro "MARGIN".
+
+# Uso:
+#
+#	lapply(X, FUN)
+#
+# Argumentos:
+# -X: Un vector o un objeto (Lista o Data frame)
+# -FUN: Función que se va a aplicar a cada elemento de X
+
+### Ejemplo sencillo 1:
+
+movies <- c("SPYDERMAN","BATMAN","VERTIGO","CHINATOWN")
+movies_lower <-lapply(movies, tolower)
+str(movies_lower)
+
+# Podemos usar *unlist* para convertir la lista en un vector.
+
+movies_lower <-unlist(lapply(movies,tolower))
+str(movies_lower)
+
+# Ejemplo sencillo 2:
+
+trees[1:5, ]
+class(trees)
+
+arboles <- lapply(X = trees, FUN = mean)
+arboles
+class(arboles)
+
+### Caso con matriz
+
+matriz <- matrix(1:9, ncol = 3)
+lapply(matriz, quantile, probs = .8)
+
+dataFrame <- as.data.frame(matriz)
+lapply(dataFrame, quantile, probs = .8)
+
+
+length(dataFrame)
+dataFrame[1]
+length(matriz)
+matriz[1]
+
+### Ejemplo con datos genómicos
 
 rpkm.df <- as.data.frame(rpkm)
 names(rpkm.df) <- c("t2", "t6", "t8", "t10")
 
-###### lappy EJECUTA LA FUNCION FUN SOBRE CADA ELEMENTO DE X
-
-##### QUIERO AGREGARLE 5 AL VALOR DE EXPRESION DE TODOS LOS GENES DEL TIEMPO 2
+# Quiero agregarle 5 al valor de expresion de todos los genes del tiempo 2
 head(rpkm.df$t2)
 lapply(head(rpkm.df$t2), function(x){x+5} )
 
-###### sappy TE REGRESA EL OBJETO MAS SIMPLE QUE PUEDE, DEPENDIENDO DEL RESULTADO DE LA FUNCION FUN
-sapply(head(rpkm.df$t2), function(x){x+5}, simplify = F)
 
+######################## sapply ########################
 
-###### QUIERO ELIMINAR EL PREFIJO "FB" DEL id DE FLYBASE PARA CADA UNO DE LOS GENES
+# La función *sapply* hace lo mismo que *lapply* pero regresa un vector en lugar
+# de una lista. "s" es de simplificar.
+
+# Uso:
+#
+#		sapply(X, FUN)
+#
+# Arguments:
+# -X: un vector o un objeto
+# -FUN: Función que se va a aplicar a cada elemento de X
+
+# Ejemplo sencillo:
+
+dt <- cars
+
+lmn_cars <- lapply(dt, min)
+smn_cars <- sapply(dt, min)
+
+lmn_cars
+smn_cars
+
+# Podemos usar una función definida por nosotros dentro *lapply* o *sapply*.
+# Creamos una función llamada *avg* que calcule el promedio de entre el máximo
+# y el mínimo de un vector:
+
+avg <- function(x) {
+	(min(x) + max(x)) / 2
+}
+
+fcars <- sapply(dt, avg)
+fcars
+
+### Ejemplo con datos genómicos 1:
+
+# Quiero eliminar el prefijo "FB" del id de flybase para cada uno de los genes.
 
 lapply(data$flybase_id, function(x){ sub("FB", "", x) })
 
-head(sapply(data$flybase_id, function(x){ sub("FB", "", x) }, simplify = T))
+sapply(data$flybase_id, function(x){ sub("FB", "", x) })
 
+sapply(data$flybase_id, function(x){ sub("FB", "", x) }, simplify = FALSE)
 
-##### SAPPLY ES BASTANTE INTELIGENTE AL DECIDIR EL TIPO DE DATO QUE NOS QUIERE REGRESAR
-##### PROBEMOS INCLUIR UNA FUNCION PERSONALIZADA ADENTRO DEL APPLY
+### Ejemplo con datos genómicos 2:
 
-#### QUEREMOS OBTENER LA EXPRESION PROMEDIO DE LOS GENES POR CROMOSOMA PARA CADA UNO DE LOS TIEMPOS
+# Queremos obtener la expresion promedio de los genes por cromosoma para cada
+# uno de los tiempos.
 
-#### NECESITAMOS:
-## 1. OBTENER TODOS LOS GENES DE UN CROMOSOMA ESPECIFICO
-## 2. OBTENER EL PROMEDIO DE LA EXPRESION PARA ESOS GENES EN EL TIEMPO 1
-## 3. OBTENER EL PROMEDIO DE LA EXPRESION PARA ESOS GENES EN EL TIEMPO 2... Y ASI SUCESIVAMENTE
-## REPETIR DE 1 A 3 PARA TODOS LOS CROMOSOMAS
+# Necesitamos:
+# 1. Obtener todos los genes de un cromosoma especifico.
+# 2. Obtener el promedio de la expresion para esos genes en el tiempo 1 (2 hrs)
+# 3. Obtener el promedio de la expresion para esos genes en el tiempo 2 (6 hrs)
+# 4. Obtener el promedio de la expresion para esos genes en el tiempo 3 (8 hrs)
+# 5. Obtener el promedio de la expresion para esos genes en el tiempo 4 (10 hrs)
+# Repetir de 1 a 5 para todos los cromosomas.
 
-
-#### PRIMERO VAMOS A GENERAR UNA FUNCION QUE HAGA LO QUE QUEREMOS PARA UN CROMOSOMA
+# Primero vamos a generar una funcion que obtenga el promedio de expresión por
+# cromosoma.
 
 cromosoma <- "chr4"
 genes.cromosoma <- subset(data, chr == cromosoma)
 tiempos <- as.matrix(cbind(genes.cromosoma$rpkm2, genes.cromosoma$rpkm6, genes.cromosoma$rpkm8, genes.cromosoma$rpkm10))
 
+
 dim(tiempos)
+head(tiempos)
 
-### QUEREMOS OBTENER LA EXPRSION PROMEDIO POR COLUMNA
-mean.expr <- apply(tiempos, 2, mean)
 
-#### CREEMOS UNA FUNCION CON LAS INSTRUCCIONES QUE ACABAMOS DE ESCRIBIR
+# Para obtener la exprsion promedio por columna.
 
-gene_mean_expr <-  function(x, data){
-  genes.cromosoma <- subset(data, chr == x)
-  tiempos <- as.matrix(cbind(genes.cromosoma$rpkm2, genes.cromosoma$rpkm6, genes.cromosoma$rpkm8, genes.cromosoma$rpkm10))
-  mean.expr <- apply(tiempos, 2, mean)
+mean.expr <- apply(X = tiempos, MARGIN = 2, FUN = mean)
+
+# Creemos una funcion con las instrucciones que acabamos de escribir
+
+gene_mean_expr <- function(x, data){
+	genes.cromosoma <- subset(data, chr == x)
+	tiempos <- as.matrix(cbind(genes.cromosoma$rpkm2, genes.cromosoma$rpkm6, genes.cromosoma$rpkm8, genes.cromosoma$rpkm10))
+	mean.expr <- apply(X = tiempos, MARGIN = 2, FUN = mean)
+	return(mean.expr)
 }
 
-
-#### HAGAMOSLO PARA TODOS LOS CROMOSOMAS UTILIZANDO UN APPLY
+# Hagamoslo para todos los cromosomas utilizando un apply
 
 cromosomas <- as.character(unique(data$chr))
 
-res_expr_perchr_lapply <- lapply(cromosomas, gene_mean_expr, data=data)
+lapply(cromosomas, gene_mean_expr, data=data)
+sapply(cromosomas, gene_mean_expr, data=data)
 
-res_expr_perchr_sapply <- sapply(cromosomas, gene_mean_expr, data=data, simplify = T)
 
 
-############
-### TAPPLY: 
-#######
+######################## tapply ########################
 
-### APLICA UNA FUNCION PARA CADA SUBCONJUNTO DE LOS DATOS
-### LOS SUBCONJUNTOS SON OBTENIDOS CON BASE EN EL FACTOR ESPECIFICADO EN INDEX
+# La función *tapply* calcula una medida (mean, median, min, max, etc.) o 
+# alguna función para cada factor en un vector.
+
+# Uso:
+#
+#	tapply(X, INDEX, FUN = NULL)
+#
+# Argumentos:
+# -X: Un objeto, usualmente un vector.
+# -INDEX: Una lista de uno o más factores del mismo tamaño que X.
+# -FUN: Función que se va aplicar a cada elemento de X por cada factor de INDEX
+
+data(iris)
+class(iris$Sepal.Width)
+class(iris$Species)
+
+tapply(X = iris$Sepal.Width, INDEX = iris$Species, FUN = median)
+
+## Ejemplo con datos genómicos
+
 
 tapply(expressed$flybase_id, expressed$chr, length)
 tapply(expressed$rpkm2, expressed$chr, mean)
 
 
 tapply(expressed$flybase_id, list(expressed$chr, expressed$strand), length)
-
 tapply(expressed$rpkm2, list(expressed$chr, expressed$strand), mean)
 
